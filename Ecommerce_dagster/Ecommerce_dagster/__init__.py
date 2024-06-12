4d200212155f41f3702fc7eecb3eb001f780d054
Long Nguyen <zedsc03@gmail.com>
2024-06-12 20:30:05 +0700

    Update definition object

---
+++ Ecommerce_dagster/Ecommerce_dagster/__init__.py
@@ -0,0 +1,23 @@
+import os
+
+from dagster import Definitions, load_assets_from_modules
+from dagster_dbt import DbtCliResource
+
+from .assets import dbt_asset, extract, load
+from .resources import postgres, snowflake
+from .constants import dbt_project_dir
+from .schedules import schedules
+
+all_assets = load_assets_from_modules(
+    [dbt_asset, extract, load])
+
+
+defs = Definitions(
+    assets=all_assets,
+    schedules=schedules,
+    resources={
+        "dbt": DbtCliResource(project_dir=os.fspath(dbt_project_dir)),
+        "psql_io_manager": postgres,
+        "snowflake": snowflake
+    },
+)
